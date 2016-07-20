8d31eb11bc8bf8ef6412137ce3ecfc22445fc46f
parth8891 <shahparth8891@gmail.com>
2016-07-20 18:41:16 -0400

    added session variable

--- client/newForm/newForm.js
+++ client/newForm/newForm.js
@@ -4,13 +4,25 @@ Template.newForm.events({
    event.preventDefault();
  
    let fname = template.find('#firstName').value;
+   let lname = template.find('#lastName').value;
+   let quote = template.find('#quote').value;
+   let gender = template.find('#gender').value;
+   let link = template.find('#githubLink').value;
 
-   console.log(fname);
-   console.log(lastName.value);
-   console.log(quote.value);
-   console.log(gender.value);
-   console.log(githubLink.value);
+   // console.log(fname);
+   // console.log(lastName.value);
+   // console.log(quote.value);
+   // console.log(gender.value);
+   // console.log(githubLink.val
 
+   Session.set('firstName',fname);
+   Session.set('lastName',lname);
+   Session.set('quote',quote);
+   Session.set('githubLink',link);
+   //Session.set('firstName',fname);
+
+   Router.go(`/${fname}`);
+   
 }
 
 });
